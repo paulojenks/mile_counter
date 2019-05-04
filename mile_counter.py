ec19d91157122045d68e78650959f7c1563e8734
Paul Jenkins <pnjenkins@gmail.com>
2019-05-04 09:44:36 -0600

    Update
    Using haversine package

--- mile_counter.py
+++ mile_counter.py
@@ -1,28 +1,28 @@
-from math import cos, asin, sqrt
 import csv
 
-
-def distance(lat1, lon1, lat2, lon2):
-    """Returns distance in miles"""
-    p = 0.017453292519943295     #Pi/180
-    a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
-    return 12742 * asin(sqrt(a))/1.609
+from haversine import haversine
 
 
 def get_distance(file):
+    """
+        Returns distance in miles between 2 GPS coordinates from a CSV file
+        Latitude must be labeled "Latitude" and Longitude must be labeled "Longitude"
+    """
     lat_list = []
     long_list = []
     with open(file) as trailer_file:
         file_reader = csv.DictReader(trailer_file, delimiter=",")
         for rows in file_reader:
-            if rows['latitude'] and rows['longitude']:
-                lat_list.append(rows['latitude'])
-                long_list.append(rows['longitude'])
+            if rows['Latitude'] and rows['Longitude']:
+                lat_list.append(rows['Latitude'])
+                long_list.append(rows['Longitude'])
     x = 0
     distances_total = 0
     while x < len(lat_list)-1:
-        y = distance(float(lat_list[x]), float(long_list[x]), float(lat_list[x+1]), float(long_list[x+1]))
+        start = (float(lat_list[x]), float(long_list[x]))
+        stop = (float(lat_list[x+1]), float(long_list[x+1]))
+        y = haversine(start, stop, unit='mi')
         distances_total += y
         x += 1
-    return distances_total
+    return round(distances_total, 0)
 
